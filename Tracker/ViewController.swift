85c8e2c77c090ce33f45b137188517b26b545c89
Owi Lover <lazygabriel@yandex.ru>
2024-11-05 20:28:03 +0300

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Owi Lover on 11/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
